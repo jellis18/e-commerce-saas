cc194dbb73426b96045cf5e73b3b6290c70e7ceb
Justin Ellis <justin.ellis18@gmail.com>
2021-05-10 15:18:20 -0400

    basic homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
-  return <div>My App 1</div>;
+  return (
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
