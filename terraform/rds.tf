3c89347f48847e40e4db297ac9ebdaa429a403ae
Justin Ellis <justin.ellis18@gmail.com>
2021-05-15 16:46:39 -0400

    changes

---
+++ terraform/rds.tf
@@ -0,0 +1,10 @@
+#resource "aws_db_instance" "db" {
+#  allocated_storage   = 10
+#  engine              = "postgres"
+#  engine_version      = "12.5"
+#  instance_class      = "db.t2.micro"
+#  name                = "proshop"
+#  username            = "postgres"
+#  password            = "postgres"
+#  skip_final_snapshot = true
+#}
